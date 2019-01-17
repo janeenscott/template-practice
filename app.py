6966ee1272ef4b036da634e0fa8cb767dfa2babf
Janeen Scott <janeen.e.scott@gmail.com>
2019-01-17 16:35:22 -0500

    song titles appear from dictionary in app.py to index.html page

--- app.py
+++ app.py
@@ -8,7 +8,64 @@ app = Flask(__name__)
 
 
 
+
 @app.route("/")
 def index():
 
-    return render_template('base.html')
+    all_songs = [
+        {'title': "Postcards",
+         'lyrics': "Now you're sending me postcards from home, pictures of the places that I know.",
+         'composer': "Janeen Scott"},
+        {'title': "Been Here Before",
+         'lyrics': "Your skin looks foreign, but I imagine it would feel like an old map.",
+         'composer': "Janeen Scott"},
+        {'title': "Barcelona",
+         'lyrics': "So we laughed at the guidebooks, and rewrote the pages",
+         'composer': "Janeen Scott"},
+        {'title': "Settle My Heart",
+         'lyrics': "How can my weary heart love a world on fire?",
+         'composer': "Janeen Scott"}
+        ]
+
+    return render_template('index.html', song_list=all_songs)
+                                        # song_list=all_songs is defining the variable,
+                                        # song_list for use in my index.html file. song_list
+                                        # is now a list of dictionaries, containing all the
+                                        # information that is listed above in all_songs.
+    # return render_template is saying "look in index.html and use the information stored in
+    # song_lists to fill in the requests in the html.
+
+
+@app.route("/postcards/")
+def postcards():
+
+    all_songs = [
+        {'title': "Postcards",
+         'lyrics': "Now you're sending me postcards from home, pictures of the places that I know.",
+         'composer': "Janeen Scott"},
+        {'title': "Been Here Before",
+         'lyrics': "Your skin looks foreign, but I imagine it would feel like an old map.",
+         'composer': "Janeen Scott"},
+        {'title': "Barcelona",
+         'lyrics': "So we laughed at the guidebooks, and rewrote the pages",
+         'composer': "Janeen Scott"},
+        {'title': "Settle My Heart",
+         'lyrics': "How can my weary heart love a world on fire?",
+         'composer': "Janeen Scott"}
+    ]
+    return render_template('postcards.html', song_list=all_songs)
+
+@app.route("/been-here-before/")
+def been_here_before():
+    pass
+
+@app.route("/barcelona/")
+def barcelona():
+    pass
+
+@app.route("/settle-my-heart/")
+def settle_my_heart():
+    pass
+
+
+
